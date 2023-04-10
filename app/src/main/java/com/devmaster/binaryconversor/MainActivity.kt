8789cf6cf7d30b9377c050ce5bc120d22717c8ba
FelipeIgansi <lipeigansi@gmail.com>
2023-04-10 17:26:36 -0300

    Converter dec to bin

--- app/src/main/java/com/devmaster/binaryconversor/MainActivity.kt
+++ app/src/main/java/com/devmaster/binaryconversor/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.devmaster.binaryconversor
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.devmaster.binaryconversor.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        binding.buttonConverter.setOnClickListener(this)
+    }
+    private fun initializeValue(str: String): Int{
+       return if (str != "") str.toInt() else 0
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_Converter) {
+            var valDecimal = initializeValue(binding.editTextDecimal.text.toString())
+            var valBinary = initializeValue(binding.editTextBinary.text.toString())
+            if (valDecimal != 0) { //decimal
+                var binary = ""
+                while (true) {
+                    binary += "${valDecimal % 2}"
+                    valDecimal /= 2
+                    if (valDecimal == 0) break
+                }
+                var a = ""
+                for (i in binary.length - 1 downTo 0) {
+                    a += binary[i]
+                }
+                binding.editTextBinary.setText(a)
+            } else if (valBinary != 0) {
+
+            } else {
+                //Apresentar erro
+            }
+        }
     }
 }
